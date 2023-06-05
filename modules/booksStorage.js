53e5ebb3d6e3ca482f829bed05c6729e5ad4bf75
rbd3 <rabedesanaa@gmail.com>
2023-06-05 09:30:30 +0300

    Delete booksStorage file

--- modules/booksStorage.js
+++
@@ -1,8 +0,0 @@
-import { BookStore } from './books.js';
-
-const newBookStorage = new BookStore();
-
-export const bookStorage = JSON.parse(localStorage.getItem('bookCollection'));
-if (bookStorage !== null) {
-  newBookStorage.store = bookStorage;
-}
